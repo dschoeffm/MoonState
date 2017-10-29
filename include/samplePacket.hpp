424d3d5e8e9c2bb37360c60f5c113f4800103d7e
Dominik Schoeffmann <schoeffm@in.tum.de>
2017-10-29 11:35:28 +0100

    add missing functions to samplePacket.hpp

--- include/samplePacket.hpp
+++ include/samplePacket.hpp
@@ -2,6 +2,7 @@
 #define SAMPLE_PACKET_HPP
 
 #include <cstdint>
+#include <cstdlib>
 
 class SamplePacket {
 private:
@@ -11,10 +12,12 @@ private:
 public:
 	SamplePacket(const SamplePacket &p) : data(p.data), dataLen(p.dataLen){};
 	SamplePacket() : data(nullptr), dataLen(0){};
-	SamplePacket(void* data, uint32_t dataLen) : data(data), dataLen(dataLen){};
+	SamplePacket(void *data, uint32_t dataLen) : data(data), dataLen(dataLen){};
+	~SamplePacket() { free(data); }
 
 	void *getData() { return data; };
 	uint32_t getDataLen() { return dataLen; };
+	void setDataLen(uint32_t l) { dataLen = l; };
 };
 
 #endif /* SAMPLE_PACKET_HPP */
