a1b83f497e1868a85d49a18fdfc141a9bb703e0b
Dominik Schoeffmann <schoeffm@in.tum.de>
2017-11-02 14:54:13 +0100

    add more elaborate HelloBye Protocol

--- tests/helloByeDriver.cpp
+++ tests/helloByeDriver.cpp
@@ -10,6 +10,7 @@
 #include <cstring>
 #include <iostream>
 #include <string>
+#include <cstdlib>
 
 #define BUFLEN 100
 
@@ -19,15 +20,17 @@ void client(int fd, struct sockaddr_in server) {
 	char buf[BUFLEN];
 	socklen_t addrlen = sizeof(struct sockaddr_in);
 
+	int cookieClient = rand()%10;
+
 	// Send first message
 	memset(buf, 0, BUFLEN);
-	sprintf(buf, "CLIENT HELLO");
+	sprintf(buf, "CLIENT HELLO:%d", cookieClient);
 	if (sendto(fd, buf, BUFLEN, 0, (const struct sockaddr *)&server, addrlen) == -1) {
 		cout << "client: sendto failed: " << std::strerror(errno) << endl;
 		abort();
 	}
 
-	cout << "Hello message away" << endl;
+	cout << "Hello message away, Cookie: " << cookieClient << endl;
 
 	// Receive first message
 	memset(buf, 0, BUFLEN);
@@ -36,16 +39,17 @@ void client(int fd, struct sockaddr_in server) {
 		abort();
 	}
 
-	if (strncmp(buf, "SERVER HELLO", 12) != 0) {
+	if (strncmp(buf, "SERVER HELLO:", 13) != 0) {
 		cout << "client: server hello failed: " << string(buf) << endl;
 		abort();
 	}
+	int cookieServer = atoi(buf + 13);
 
-	cout << "Server hello received" << endl;
+	cout << "Server hello received, Cookie: " << cookieServer << endl;
 
 	// Send bye message
 	memset(buf, 0, BUFLEN);
-	sprintf(buf, "CLIENT BYE");
+	sprintf(buf, "CLIENT BYE:%d", cookieServer);
 	if (sendto(fd, buf, BUFLEN, 0, (const struct sockaddr *)&server, addrlen) == -1) {
 		cout << "client: sendto failed: " << std::strerror(errno) << endl;
 		abort();
@@ -60,8 +64,13 @@ void client(int fd, struct sockaddr_in server) {
 		abort();
 	}
 
-	if (strncmp(buf, "SERVER BYE", 10) != 0) {
+	if (strncmp(buf, "SERVER BYE:", 10) != 0) {
 		cout << "client: server bye failed: " << string(buf) << endl;
+		abort();
+	}
+
+	if(cookieClient != atoi(buf + 11)){
+		cout << "client: server sent me the wrong cookie... I want chocolate..." << endl;
 		abort();
 	}
 
@@ -86,38 +95,47 @@ void server(int fd, struct sockaddr_in server) {
 
 	cout << "Waiting for client" << endl;
 
+	int cookieServer = rand()%10;
+
 	// Receive client hello
 	if (recvfrom(fd, buf, BUFLEN, 0, (struct sockaddr *)&client, &clen) < 0) {
 		cout << "server: recvfrom failed: " << std::strerror(errno) << endl;
 	}
 
-	if (strncmp(buf, "CLIENT HELLO", 12) != 0) {
+	if (strncmp(buf, "CLIENT HELLO:", 13) != 0) {
 		cout << "server: client hello failed: " << std::strerror(errno) << endl;
 		abort();
 	}
 
-	cout << "Client hello received" << endl;
+	int cookieClient = atoi(buf + 13);
+
+	cout << "Client hello received, Cookie:" << cookieClient << endl;
 	cout << "Received packet from: " << inet_ntoa(client.sin_addr) << ":"
 		 << ntohs(client.sin_port) << endl;
 	assert(client.sin_family == AF_INET);
 
 	// Send first message
 	memset(buf, 0, BUFLEN);
-	sprintf(buf, "SERVER HELLO");
+	sprintf(buf, "SERVER HELLO:%d", cookieServer);
 	if (sendto(fd, buf, BUFLEN, 0, (const struct sockaddr *)&client, sizeof(client)) == -1) {
 		cout << "server: sendto failed: " << std::strerror(errno) << endl;
 		abort();
 	}
 
-	cout << "Hello message away" << endl;
+	cout << "Hello message away, Cookie:" << cookieServer << endl;
 
 	// Receive client bye
 	if (recvfrom(fd, buf, BUFLEN, 0, (struct sockaddr *)&client, &clen) < 0) {
 		cout << "server: recvfrom failed: " << std::strerror(errno) << endl;
 	}
 
-	if (strncmp(buf, "CLIENT BYE", 10) != 0) {
+	if (strncmp(buf, "CLIENT BYE:", 10) != 0) {
 		cout << "server: client bye failed" << endl;
+		abort();
+	}
+
+	if(cookieServer != atoi(buf + 11)){
+		cout << "server: client sent me the wrong cookie... I want vanilla..." << endl;
 		abort();
 	}
 
@@ -125,7 +143,7 @@ void server(int fd, struct sockaddr_in server) {
 
 	// Send server bye
 	memset(buf, 0, BUFLEN);
-	sprintf(buf, "SERVER BYE");
+	sprintf(buf, "SERVER BYE:%d", cookieClient);
 	if (sendto(fd, buf, BUFLEN, 0, (const struct sockaddr *)&client, addrlen) == -1) {
 		cout << "server: sendto failed: " << std::strerror(errno) << endl;
 		abort();
@@ -148,6 +166,8 @@ int main(int argc, char **argv) {
 	struct sockaddr_in sa;
 	bool c;
 	uint16_t port;
+
+	srand (time(NULL));
 
 	memset((char *)&sa, 0, sizeof(struct sockaddr_in));
 	sa.sin_family = AF_INET;
