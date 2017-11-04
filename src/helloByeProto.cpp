e7fc9eb329c7926f8bf4b1529ee3df46c633f4eb
Dominik Schoeffmann <schoeffm@in.tum.de>
2017-11-04 16:00:05 +0100

    HelloByeClientHello::fun()

--- src/helloByeProto.cpp
+++ src/helloByeProto.cpp
@@ -121,6 +121,50 @@ void HelloByeServerBye<Identifier, Packet>::fun(
 	funIface.addPktToSend(pkt);
 };
 
+/*
+ * ===================================
+ * HelloByeClientHello
+ * ===================================
+ *
+ */
+
+template <class Identifier, class Packet>
+HelloByeClientHello<Identifier, Packet>::HelloByeClientHello() {
+	this->clientCookie = rand();
+};
+
+template <class Identifier, class Packet>
+void HelloByeClientHello<Identifier, Packet>::fun(
+	typename SM::State &state, Packet *pkt, typename SM::FunIface &funIface) {
+
+	// Get info from packet
+	Ethernet *ether = reinterpret_cast<Ethernet *>(pkt->getData());
+	IPv4 *ipv4 = reinterpret_cast<IPv4 *>(ether->getPayload());
+	Udp *udp = reinterpret_cast<Udp *>(ipv4->getPayload());
+
+	// Prepare new packet
+	// Set payload string
+	stringstream sstream;
+	sstream << "SERVER HELLO:" << this->clientCookie << endl;
+	string serverHelloStr = sstream.str();
+	memcpy(udp->getPayload(), serverHelloStr.c_str(), serverHelloStr.length());
+
+	// Set the IP header stuff
+	// Leave the payload length alone for now...
+	ipv4->ip_ttl = 64;
+	uint32_t tmp = ipv4->ip_dst.s_addr;
+	ipv4->ip_dst.s_addr = ipv4->ip_src.s_addr;
+	ipv4->ip_src.s_addr = tmp;
+	ipv4->calcChecksum();
+
+	// Set UDP checksum to 0 and hope for the best
+	udp->check = 0;
+
+	state.transition(HelloByeClient::Bye);
+
+	funIface.addPktToSend(pkt);
+};
+
 template class HelloByeServerHello<IPv4_5TupleL2Ident<SamplePacket>, SamplePacket>;
 template class HelloByeServerBye<IPv4_5TupleL2Ident<SamplePacket>, SamplePacket>;
 template class HelloByeClientHello<IPv4_5TupleL2Ident<SamplePacket>, SamplePacket>;
