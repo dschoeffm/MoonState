e049597c70cb012d3b23ce63ad8842698f41ce82
Dominik Schoeffmann <schoeffm@in.tum.de>
2017-11-14 17:21:54 +0100

    add DPDK mbuf datastructure

---
+++ include/mbuf.hpp
@@ -0,0 +1,19 @@
+#ifndef MBUF_HPP
+#define MBUF_HPP
+
+#ifdef WITH_DPDK
+
+#include <cstdint>
+#include <rte_mbuf.h>
+
+struct mbuf : public rte_mbuf {
+	void *getData() { return this->buf_addr; }
+	uint16_t getDataLen() { return this->data_len; };
+	void setDataLen(uint16_t l) { this->data_len = l; };
+	uint16_t getBufLen() { return this->buf_len; }
+};
+
+
+#endif /* WITH_DPDK */
+
+#endif /* MBUF_HPP */
