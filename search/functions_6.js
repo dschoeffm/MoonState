f33a5d47f21fb178124c6860b992ed1bc05081db
Dominik Schoeffmann <schoeffm@in.tum.de>
2017-11-22 18:19:42 +0100

    doxygen update

--- search/functions_6.js
+++ search/functions_6.js
@@ -1,5 +1,9 @@
 var searchData=
 [
+  ['hellobyeclient_5fconnect',['HelloByeClient_connect',['../helloBye__C_8hpp.html#aa41942010182ac52d8f059be2891c54d',1,'helloBye_C.hpp']]],
+  ['hellobyeclient_5ffree',['HelloByeClient_free',['../helloBye__C_8hpp.html#a1a2c76acf4ed944debcda4e42456f38a',1,'helloBye_C.hpp']]],
+  ['hellobyeclient_5finit',['HelloByeClient_init',['../helloBye__C_8hpp.html#a50fcff4cd28a4445da1d9247087d3850',1,'helloBye_C.hpp']]],
+  ['hellobyeclient_5fprocess',['HelloByeClient_process',['../helloBye__C_8hpp.html#ab1d7c701025631165885fec5c1aa5bcd',1,'helloBye_C.hpp']]],
   ['hellobyeclientbye',['HelloByeClientBye',['../classHelloBye_1_1HelloByeClientBye.html#a099651429e276fd2bec617e922bd992e',1,'HelloBye::HelloByeClientBye']]],
   ['hellobyeclienthello',['HelloByeClientHello',['../classHelloBye_1_1HelloByeClientHello.html#ac9a37b994e7509d580b9f6ed47bcf9c3',1,'HelloBye::HelloByeClientHello']]],
   ['hellobyeclientrecvbye',['HelloByeClientRecvBye',['../classHelloBye_1_1HelloByeClientRecvBye.html#a03208c886950b88440ea171341d2df5e',1,'HelloBye::HelloByeClientRecvBye']]],
