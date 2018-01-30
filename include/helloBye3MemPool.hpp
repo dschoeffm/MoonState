f48ede21d362f5603a5b0a6218c785a7f9a1f0ce
Dominik Schoeffmann <schoeffm@in.tum.de>
2018-01-30 12:43:35 +0100

    Add helloBye3MemPool
    This is basically the same as helloBye3, but it uses rte_mempool instead
    of new()/delete()

---
+++ include/helloBye3MemPool.hpp
@@ -0,0 +1,261 @@
+#ifndef HELLOBYE3MEMPOOLPROTO_HPP
+#define HELLOBYE3MEMPOOLPROTO_HPP
+
+#include <mutex>
+#include <sstream>
+
+#include "common.hpp"
+#include "exceptions.hpp"
+#include "headers.hpp"
+#include "mbuf.hpp"
+#include "stateMachine.hpp"
+
+namespace HelloBye3MemPool {
+
+struct __attribute__((packed)) msg {
+	uint64_t ident;
+	uint8_t role;
+	uint8_t msg;
+	uint8_t cookie;
+
+	static constexpr uint8_t ROLE_CLIENT = 0;
+	static constexpr uint8_t ROLE_SERVER = 1;
+
+	static constexpr uint8_t MSG_HELLO = 0;
+	static constexpr uint8_t MSG_BYE = 1;
+};
+
+/*
+ * ===================================
+ * Identifier
+ * ===================================
+ *
+ */
+
+// This is the same as in HelloBye2
+template <typename Packet> class Identifier {
+public:
+	class ConnectionID {
+	public:
+		uint64_t ident;
+
+		bool operator==(const ConnectionID &c) const { return ident == c.ident; };
+		bool operator<(const ConnectionID &c) const { return ident < c.ident; };
+		operator std::string() const {
+			std::stringstream sstream;
+			sstream << ident;
+			return sstream.str();
+		};
+		ConnectionID(const ConnectionID &c) : ident(c.ident){};
+		ConnectionID() : ident(0){};
+		ConnectionID(uint64_t i) : ident(i){};
+	};
+	struct Hasher {
+		uint64_t operator()(const ConnectionID &c) const { return c.ident; };
+	};
+	static ConnectionID identify(Packet *pkt) {
+		Headers::Ethernet *eth = reinterpret_cast<Headers::Ethernet *>(pkt->getData());
+		if (eth->getEthertype() != Headers::Ethernet::ETHERTYPE_IPv4) {
+			throw new PacketNotIdentified();
+		}
+
+		Headers::IPv4 *ipv4 = reinterpret_cast<Headers::IPv4 *>(eth->getPayload());
+		if (ipv4->proto != Headers::IPv4::PROTO_UDP) {
+			throw new PacketNotIdentified();
+		}
+
+		Headers::Udp *udp = reinterpret_cast<Headers::Udp *>(ipv4->getPayload());
+
+		struct msg *msg = reinterpret_cast<struct msg *>(udp->getPayload());
+		return msg->ident;
+	};
+};
+
+/*
+ * ===================================
+ * Forward declarations
+ * ===================================
+ *
+ */
+
+/*
+ * ===================================
+ * Server
+ * ===================================
+ *
+ */
+
+namespace Server {
+struct server {
+	int clientCookie;
+	int serverCookie;
+};
+struct States {
+	static constexpr StateID Hello = 0;
+	static constexpr StateID Bye = 1;
+	static constexpr StateID Terminate = 2;
+};
+
+void *factory(Identifier<mbuf>::ConnectionID id);
+
+void runHello(StateMachine<Identifier<mbuf>, mbuf>::State &state, mbuf *pkt,
+	StateMachine<Identifier<mbuf>, mbuf>::FunIface &funIface);
+
+void runBye(StateMachine<Identifier<mbuf>, mbuf>::State &state, mbuf *pkt,
+	StateMachine<Identifier<mbuf>, mbuf>::FunIface &funIface);
+
+}; // namespace Server
+
+/*
+ * ===================================
+ * Client
+ * ===================================
+ *
+ */
+
+namespace Client {
+struct client {
+	uint64_t ident;
+	uint32_t srcIp;
+	uint32_t dstIp;
+	int clientCookie;
+	int serverCookie;
+	uint16_t srcPort;
+	uint16_t dstPort;
+};
+
+struct States {
+	static constexpr StateID Hello = 0;
+	static constexpr StateID Bye = 1;
+	static constexpr StateID RecvBye = 2;
+	static constexpr StateID Terminate = 3;
+};
+
+StateMachine<Identifier<mbuf>, mbuf>::State createHello(
+	uint32_t srcIp, uint32_t dstIp, uint16_t srcPort, uint16_t dstPort, uint64_t ident);
+
+void runHello(StateMachine<Identifier<mbuf>, mbuf>::State &state, mbuf *pkt,
+	StateMachine<Identifier<mbuf>, mbuf>::FunIface &funIface);
+
+void runBye(StateMachine<Identifier<mbuf>, mbuf>::State &state, mbuf *pkt,
+	StateMachine<Identifier<mbuf>, mbuf>::FunIface &funIface);
+
+void runRecvBye(StateMachine<Identifier<mbuf>, mbuf>::State &state, mbuf *pkt,
+	StateMachine<Identifier<mbuf>, mbuf>::FunIface &funIface);
+
+} // namespace Client
+
+}; // namespace HelloBye3MemPool
+
+/*
+ * From here on down is the C interface to the above C++ functions
+ */
+
+extern "C" {
+
+/*
+ * XXX -------------------------------------------- XXX
+ *       Server
+ * XXX -------------------------------------------- XXX
+ */
+
+/*! Init a HelloBye server
+ *
+ *	\return void* to the object (opaque)
+ */
+void *HelloBye3MemPool_Server_init();
+
+/*! Process a batch of packets
+ *
+ * \param obj object returned by HelloByeServer_init()
+ * \param inPkts incoming packets to process
+ * \param inCount number of incoming packets
+ * \param sendCount Number of packets in sendPkts
+ * \param freeCount Number of packets in freePkts
+ */
+void *HelloBye3MemPool_Server_process(void *obj, struct rte_mbuf **inPkts,
+	unsigned int inCount, unsigned int *sendCount, unsigned int *freeCount);
+
+/*! Get the packets to send and free
+ *
+ * \param obj object returned by HelloByeServer_process
+ * \param sendPkts Packets to send out (size if given in sendCount)
+ * \param freePkts Packets to free (size if given in freeCount)
+ */
+void HelloBye3MemPool_Server_getPkts(
+	void *obj, struct rte_mbuf **sendPkts, struct rte_mbuf **freePkts);
+
+/*! Free recources used by the state machine
+ *
+ * \param obj object returned by HelloByeServer_init()
+ */
+void HelloBye3MemPool_Server_free(void *obj);
+
+/*
+ * XXX -------------------------------------------- XXX
+ *       Client
+ * XXX -------------------------------------------- XXX
+ */
+
+/*! Init a HelloBye client
+ *
+ * \return void* to the object (opaque)
+ */
+void *HelloBye3MemPool_Client_init();
+
+/*! Open a connection to a server
+ *
+ * \param obj object returned by HelloBye3MemPool_Client_init()
+ * \param inPkts incoming packets to process
+ * \param inCount number of incoming packets
+ * \param sendCount Number of packets in sendPkts
+ * \param freeCount Number of packets in freePkts
+ * \param srcIP IP of the sender
+ * \param dstIP IP of the listening server
+ * \param srcPort Port the client should use
+ * \param dstPort Port of the server
+ * \param ident Identiry of the connection
+ * \return void* to BufArray object (give to _getPkts() )
+ */
+void *HelloBye3MemPool_Client_connect(void *obj, struct rte_mbuf **inPkts,
+	unsigned int inCount, unsigned int *sendCount, unsigned int *freeCount, uint32_t srcIP,
+	uint32_t dstIP, uint16_t srcPort, uint16_t dstPort, uint64_t ident);
+
+/*! Get the packets to send and free
+ *
+ * \param obj object returned by HelloBye3MemPool_Client_process
+ * \param sendPkts Packets to send out (size if given in sendCount)
+ * \param freePkts Packets to free (size if given in freeCount)
+ */
+void HelloBye3MemPool_Client_getPkts(
+	void *obj, struct rte_mbuf **sendPkts, struct rte_mbuf **freePkts);
+
+/*! Process a batch of packets
+ *
+ * \param obj object returned by HelloBye3MemPool_Client_init()
+ * \param inPkts incoming packets to process
+ * \param inCount number of incoming packets
+ * \param sendCount Number of packets in sendPkts
+ * \param freeCount Number of packets in freePkts
+ * \return void* to BufArray object (give to _getPkts() )
+ */
+void *HelloBye3MemPool_Client_process(void *obj, struct rte_mbuf **inPkts,
+	unsigned int inCount, unsigned int *sendCount, unsigned int *freeCount);
+
+/*! Free recources used by the state machine
+ *
+ * \param obj object returned by HelloBye3MemPool_Client_init()
+ */
+void HelloBye3MemPool_Client_free(void *obj);
+};
+
+/*
+ * YCM definition is a workaround for a libclang bug
+ * When compiling, YCM should never be set.
+ * Set YCM in a libclang based IDE in order to avoid errors
+ */
+#ifndef YCM
+#include "../src/helloBye3MemPool.cpp"
+#endif
+
+#endif /* HELLOBYE3MEMPOOLPROTO_HPP */
---
+++ src/helloBye3MemPool.cpp
@@ -0,0 +1,471 @@
+#ifndef HELLOBYE3PROTO_CPP
+#define HELLOBYE3PROTO_CPP
+
+#include <cstdlib>
+#include <sstream>
+#include <stdexcept>
+
+#include <rte_errno.h>
+#include <rte_mempool.h>
+
+#include "IPv4_5TupleL2Ident.hpp"
+#include "headers.hpp"
+#include "helloBye3MemPool.hpp"
+#include "mbuf.hpp"
+#include "samplePacket.hpp"
+
+// using namespace Headers;
+// using namespace std;
+
+namespace HelloBye3MemPool {
+
+namespace Server {
+
+static struct rte_mempool *mempool;
+static std::mutex mempoolMtx;
+
+void *factory(Identifier<mbuf>::ConnectionID id) {
+	(void)id;
+	struct server *s;
+	if (rte_mempool_get(mempool, reinterpret_cast<void **>(&s)) != 0) {
+		throw std::runtime_error("HelloBye3MemPool::Server::factory() mempool is empty");
+	}
+	s->serverCookie = rand() % 256;
+	s->clientCookie = 0;
+	return s;
+};
+
+void runHello(StateMachine<Identifier<mbuf>, mbuf>::State &state, mbuf *pkt,
+	StateMachine<Identifier<mbuf>, mbuf>::FunIface &funIface) {
+
+	server *s = reinterpret_cast<struct server *>(state.stateData);
+
+	// Get info from packet
+	Headers::Ethernet *ether = reinterpret_cast<Headers::Ethernet *>(pkt->getData());
+	Headers::IPv4 *ipv4 = reinterpret_cast<Headers::IPv4 *>(ether->getPayload());
+	Headers::Udp *udp = reinterpret_cast<Headers::Udp *>(ipv4->getPayload());
+
+	struct msg *msg = reinterpret_cast<struct msg *>(udp->getPayload());
+
+	D(std::cout << "HelloBye3MemPool::Server::runHello() pkt: " << (void *)pkt->getData()
+				<< ", ident: " << msg->ident << std::endl;)
+
+	if (msg->role != 0) {
+		//		std::abort();
+		std::cout
+			<< "HelloBye3MemPool::Server::runHello() client hello wrong - role not client"
+			<< std::endl;
+		funIface.transition(States::Terminate);
+		rte_mempool_put(mempool, reinterpret_cast<void *>(s));
+		funIface.freePkt();
+		return;
+	}
+
+	if (msg->msg != 0) {
+		//		std::abort();
+		std::cout << "HelloBye3MemPool::Server::runHello() client hello wrong - msg not hello"
+				  << std::endl;
+		funIface.transition(States::Terminate);
+		rte_mempool_put(mempool, reinterpret_cast<void *>(s));
+		funIface.freePkt();
+		return;
+	}
+
+	// Get client cookie
+	s->clientCookie = msg->cookie;
+	D(std::cout << "HelloBye3MemPool::Server::runHello() clientCookie: "
+				<< static_cast<int>(s->clientCookie) << std::endl;)
+
+	msg->role = msg::ROLE_SERVER;
+	msg->msg = msg::MSG_HELLO;
+	msg->cookie = s->serverCookie;
+
+	// Set the IP header stuff
+	// Leave the payload length alone for now...
+
+	ipv4->ttl = 64;
+	uint32_t tmp = ipv4->dstIP;
+	ipv4->dstIP = ipv4->srcIP;
+	ipv4->srcIP = tmp;
+	ipv4->checksum = 0;
+
+	// Set UDP checksum to 0 and hope for the best
+
+	udp->checksum = 0;
+	uint16_t tmp16 = udp->dstPort;
+	udp->dstPort = udp->srcPort;
+	udp->srcPort = tmp16;
+
+	funIface.transition(States::Bye);
+};
+
+void runBye(StateMachine<Identifier<mbuf>, mbuf>::State &state, mbuf *pkt,
+	StateMachine<Identifier<mbuf>, mbuf>::FunIface &funIface) {
+
+	server *s = reinterpret_cast<struct server *>(state.stateData);
+
+	// Get info from packet
+	Headers::Ethernet *ether = reinterpret_cast<Headers::Ethernet *>(pkt->getData());
+	Headers::IPv4 *ipv4 = reinterpret_cast<Headers::IPv4 *>(ether->getPayload());
+	Headers::Udp *udp = reinterpret_cast<Headers::Udp *>(ipv4->getPayload());
+
+	struct msg *msg = reinterpret_cast<struct msg *>(udp->getPayload());
+
+	if ((msg->role != msg::ROLE_CLIENT) || (msg->msg != msg::MSG_BYE)) {
+		std::cout << "HelloBye3MemPool::Server::runBye() msg fields wrong" << std::endl;
+		funIface.transition(States::Terminate);
+		rte_mempool_put(mempool, reinterpret_cast<void *>(s));
+		funIface.freePkt();
+		return;
+	}
+
+	if (msg->cookie != s->serverCookie) {
+		std::cout << "HelloBye3MemPool::Server::runBye() Client sent over wrong cookie"
+				  << std::endl;
+		funIface.transition(States::Terminate);
+		rte_mempool_put(mempool, reinterpret_cast<void *>(s));
+		funIface.freePkt();
+		return;
+	}
+
+	// Prepare new packet
+	msg->cookie = s->clientCookie;
+	msg->role = msg::ROLE_SERVER;
+	msg->msg = msg::MSG_BYE;
+
+	// Set the IP header stuff
+	// Leave the payload length alone for now...
+	ipv4->ttl = 64;
+	uint32_t tmp = ipv4->dstIP;
+	ipv4->dstIP = ipv4->srcIP;
+	ipv4->srcIP = tmp;
+	ipv4->checksum = 0;
+
+	// Set UDP checksum to 0 and hope for the best
+	udp->checksum = 0;
+	uint16_t tmp16 = udp->dstPort;
+	udp->dstPort = udp->srcPort;
+	udp->srcPort = tmp16;
+
+	// We are done after this -> transition to Terminate
+	funIface.transition(States::Terminate);
+	rte_mempool_put(mempool, reinterpret_cast<void *>(s));
+};
+}; // namespace Server
+
+namespace Client {
+
+static struct rte_mempool *mempool;
+static std::mutex mempoolMtx;
+
+StateMachine<Identifier<mbuf>, mbuf>::State createHello(
+	uint32_t srcIp, uint32_t dstIp, uint16_t srcPort, uint16_t dstPort, uint64_t ident) {
+
+	struct client *c;
+	if (rte_mempool_get(mempool, reinterpret_cast<void **>(&c)) != 0) {
+		throw std::runtime_error("HelloBye3MemPool::Client::createHello() mempool is empty");
+	}
+
+	c->srcIp = srcIp;
+	c->dstIp = dstIp;
+	c->srcPort = srcPort;
+	c->dstPort = dstPort;
+	c->ident = ident;
+	c->clientCookie = rand() % 256;
+	c->serverCookie = 0;
+
+	StateMachine<Identifier<mbuf>, mbuf>::State state(
+		States::Hello, reinterpret_cast<void *>(c));
+	return state;
+};
+
+void runHello(StateMachine<Identifier<mbuf>, mbuf>::State &state, mbuf *pkt,
+	StateMachine<Identifier<mbuf>, mbuf>::FunIface &funIface) {
+
+	client *c = reinterpret_cast<struct client *>(state.stateData);
+
+	memset(pkt->getData(), 0, pkt->getDataLen());
+
+	// Get info from packet
+	Headers::Ethernet *ether = reinterpret_cast<Headers::Ethernet *>(pkt->getData());
+	Headers::IPv4 *ipv4 = reinterpret_cast<Headers::IPv4 *>(ether->getPayload());
+
+	// Set the IP header stuff
+	// Leave the payload length alone for now...
+	ipv4->setVersion();
+	ipv4->setIHL(5);
+	ipv4->ttl = 64;
+	ipv4->setLength(100 - 14);
+	ipv4->setDstIP(c->dstIp);
+	ipv4->setSrcIP(c->srcIp);
+	ipv4->setProtoUDP();
+	ipv4->checksum = 0;
+
+	Headers::Udp *udp = reinterpret_cast<Headers::Udp *>(ipv4->getPayload());
+	struct msg *msg = reinterpret_cast<struct msg *>(udp->getPayload());
+
+	// Prepare msg
+	msg->ident = c->ident;
+	msg->role = msg::ROLE_CLIENT;
+	msg->cookie = c->clientCookie;
+	msg->msg = msg::MSG_HELLO;
+
+	ether->ethertype = htons(0x0800);
+
+	// Set UDP checksum to 0 and hope for the best
+	udp->checksum = 0;
+	udp->setDstPort(c->dstPort);
+	udp->setSrcPort(c->srcPort);
+	udp->setPayloadLength(50);
+
+	funIface.transition(States::Bye);
+};
+
+void runBye(StateMachine<Identifier<mbuf>, mbuf>::State &state, mbuf *pkt,
+	StateMachine<Identifier<mbuf>, mbuf>::FunIface &funIface) {
+
+	client *c = reinterpret_cast<struct client *>(state.stateData);
+
+	// Get info from packet
+	Headers::Ethernet *ether = reinterpret_cast<Headers::Ethernet *>(pkt->getData());
+	Headers::IPv4 *ipv4 = reinterpret_cast<Headers::IPv4 *>(ether->getPayload());
+	Headers::Udp *udp = reinterpret_cast<Headers::Udp *>(ipv4->getPayload());
+
+	struct msg *msg = reinterpret_cast<struct msg *>(udp->getPayload());
+
+	D(std::cout << "HelloBye3MemPool::Client::runBye() function called, ident:" << msg->ident
+				<< std::endl;)
+
+	if ((msg->role != msg::ROLE_SERVER) || (msg->msg != msg::MSG_HELLO)) {
+		std::cout << "HelloBye3MemPool::Client::runBye() msg fields wrong" << std::endl;
+		funIface.transition(States::Terminate);
+		rte_mempool_put(mempool, reinterpret_cast<void *>(c));
+		funIface.freePkt();
+		return;
+	}
+
+	// Get server cookie
+	c->serverCookie = msg->cookie;
+
+	msg->cookie = c->serverCookie;
+	msg->role = msg::ROLE_CLIENT;
+	msg->msg = msg::MSG_BYE;
+
+	// Set the IP header stuff
+	// Leave the payload length alone for now...
+	ipv4->ttl = 64;
+	uint32_t tmp = ipv4->dstIP;
+	ipv4->dstIP = ipv4->srcIP;
+	ipv4->srcIP = tmp;
+	ipv4->checksum = 0;
+
+	// Set UDP checksum to 0 and hope for the best
+	udp->checksum = 0;
+	uint16_t tmp16 = udp->dstPort;
+	udp->dstPort = udp->srcPort;
+	udp->srcPort = tmp16;
+
+	D(std::cout << "HelloBye3MemPool::Client::runBye() Dump of outgoing packet" << std::endl;)
+	D(hexdump(pkt->getData(), 64);)
+
+	// We need to wait for the server reply
+	funIface.transition(States::RecvBye);
+};
+
+void runRecvBye(StateMachine<Identifier<mbuf>, mbuf>::State &state, mbuf *pkt,
+	StateMachine<Identifier<mbuf>, mbuf>::FunIface &funIface) {
+
+	client *c = reinterpret_cast<struct client *>(state.stateData);
+
+	// Get info from packet
+	Headers::Ethernet *ether = reinterpret_cast<Headers::Ethernet *>(pkt->getData());
+	Headers::IPv4 *ipv4 = reinterpret_cast<Headers::IPv4 *>(ether->getPayload());
+	Headers::Udp *udp = reinterpret_cast<Headers::Udp *>(ipv4->getPayload());
+
+	struct msg *msg = reinterpret_cast<struct msg *>(udp->getPayload());
+
+	if ((msg->role != msg::ROLE_SERVER) || (msg->msg != msg::MSG_BYE)) {
+		std::cout << "HelloBye3MemPool::Client::runRecvBye() msg fields wrong" << std::endl;
+		funIface.transition(States::Terminate);
+		rte_mempool_put(mempool, reinterpret_cast<void *>(c));
+		funIface.freePkt();
+		return;
+	}
+
+	if (msg->cookie != c->clientCookie) {
+		std::cout << "HelloBye3MemPool::Client::runRecvBye() Server sent over wrong cookie"
+				  << std::endl;
+		std::cout << "Expected: " << static_cast<int>(c->clientCookie);
+		std::cout << ", Got: " << static_cast<int>(msg->cookie) << std::endl;
+		funIface.transition(States::Terminate);
+		rte_mempool_put(mempool, reinterpret_cast<void *>(c));
+		funIface.freePkt();
+		return;
+	}
+
+	funIface.freePkt();
+
+	// We are done after this -> transition to Terminate
+	funIface.transition(States::Terminate);
+	rte_mempool_put(mempool, reinterpret_cast<void *>(c));
+};
+}; // namespace Client
+}; // namespace HelloBye3MemPool
+
+extern "C" {
+
+/*
+ * Server
+ */
+
+void *HelloBye3MemPool_Server_init() {
+
+	srand(time(NULL));
+
+	HelloBye3MemPool::Server::mempoolMtx.lock();
+	if (HelloBye3MemPool::Server::mempool == nullptr) {
+		HelloBye3MemPool::Server::mempool = rte_mempool_create("Server MemPool", 8192,
+			sizeof(HelloBye3MemPool::Server::server), 128, 0, NULL, NULL, NULL, NULL,
+			SOCKET_ID_ANY, 0);
+		if (HelloBye3MemPool::Server::mempool == nullptr) {
+			std::cout << "HelloBye3MemPool_Server_init() mempool creation failed"
+					  << std::endl;
+			std::cout << "Error: " << rte_strerror(rte_errno) << std::endl;
+			std::abort();
+		}
+	}
+	HelloBye3MemPool::Server::mempoolMtx.unlock();
+
+	auto *obj = new StateMachine<HelloBye3MemPool::Identifier<mbuf>, mbuf>();
+
+	obj->registerEndStateID(HelloBye3MemPool::Server::States::Terminate);
+	obj->registerStartStateID(
+		HelloBye3MemPool::Server::States::Hello, HelloBye3MemPool::Server::factory);
+
+	obj->registerFunction(
+		HelloBye3MemPool::Server::States::Hello, HelloBye3MemPool::Server::runHello);
+	obj->registerFunction(
+		HelloBye3MemPool::Server::States::Bye, HelloBye3MemPool::Server::runBye);
+
+	return obj;
+};
+
+void *HelloBye3MemPool_Server_process(void *obj, struct rte_mbuf **inPkts,
+	unsigned int inCount, unsigned int *sendCount, unsigned int *freeCount) {
+
+	BufArray<mbuf> *inPktsBA =
+		new BufArray<mbuf>(reinterpret_cast<mbuf **>(inPkts), inCount, true);
+
+	auto *sm =
+		reinterpret_cast<StateMachine<HelloBye3MemPool::Identifier<mbuf>, mbuf> *>(obj);
+	sm->runPktBatch(*inPktsBA);
+	*sendCount = inPktsBA->getSendCount();
+	*freeCount = inPktsBA->getFreeCount();
+
+	return inPktsBA;
+};
+
+void HelloBye3MemPool_Server_getPkts(
+	void *obj, struct rte_mbuf **sendPkts, struct rte_mbuf **freePkts) {
+	BufArray<mbuf> *inPktsBA = reinterpret_cast<BufArray<mbuf> *>(obj);
+
+	inPktsBA->getSendBufs(reinterpret_cast<mbuf **>(sendPkts));
+	inPktsBA->getFreeBufs(reinterpret_cast<mbuf **>(freePkts));
+
+	delete (inPktsBA);
+};
+
+void HelloBye3MemPool_Server_free(void *obj) {
+	delete (reinterpret_cast<StateMachine<HelloBye3MemPool::Identifier<mbuf>, mbuf> *>(obj));
+	rte_mempool_free(HelloBye3MemPool::Server::mempool);
+};
+
+/*
+ * Client
+ */
+
+void *HelloBye3MemPool_Client_init() {
+
+	srand(time(NULL));
+
+	HelloBye3MemPool::Client::mempoolMtx.lock();
+	if (HelloBye3MemPool::Client::mempool == nullptr) {
+		HelloBye3MemPool::Client::mempool = rte_mempool_create("Client MemPool", 8192,
+			sizeof(HelloBye3MemPool::Client::client), 128, 0, NULL, NULL, NULL, NULL,
+			SOCKET_ID_ANY, 0);
+		if (HelloBye3MemPool::Client::mempool == nullptr) {
+			std::cout << "HelloBye3MemPool_Client_init() mempool creation failed"
+					  << std::endl;
+			std::cout << "Error: " << rte_strerror(rte_errno) << std::endl;
+			std::abort();
+		}
+	}
+	HelloBye3MemPool::Client::mempoolMtx.unlock();
+
+	auto *obj = new StateMachine<HelloBye3MemPool::Identifier<mbuf>, mbuf>();
+
+	obj->registerEndStateID(HelloBye3MemPool::Client::States::Terminate);
+
+	obj->registerFunction(
+		HelloBye3MemPool::Client::States::Hello, HelloBye3MemPool::Client::runHello);
+	obj->registerFunction(
+		HelloBye3MemPool::Client::States::Bye, HelloBye3MemPool::Client::runBye);
+	obj->registerFunction(
+		HelloBye3MemPool::Client::States::RecvBye, HelloBye3MemPool::Client::runRecvBye);
+
+	return obj;
+};
+
+void *HelloBye3MemPool_Client_connect(void *obj, struct rte_mbuf **inPkts,
+	unsigned int inCount, unsigned int *sendCount, unsigned int *freeCount, uint32_t srcIP,
+	uint32_t dstIP, uint16_t srcPort, uint16_t dstPort, uint64_t ident) {
+
+	auto *sm =
+		reinterpret_cast<StateMachine<HelloBye3MemPool::Identifier<mbuf>, mbuf> *>(obj);
+
+	BufArray<mbuf> *inPktsBA =
+		new BufArray<mbuf>(reinterpret_cast<mbuf **>(inPkts), inCount, true);
+
+	HelloBye3MemPool::Identifier<mbuf>::ConnectionID cID;
+	cID.ident = ident;
+
+	auto state = HelloBye3MemPool::Client::createHello(srcIP, dstIP, srcPort, dstPort, ident);
+
+	sm->addState(cID, state, *inPktsBA);
+
+	*sendCount = inPktsBA->getSendCount();
+	*freeCount = inPktsBA->getFreeCount();
+
+	return inPktsBA;
+};
+
+void HelloBye3MemPool_Client_getPkts(
+	void *obj, struct rte_mbuf **sendPkts, struct rte_mbuf **freePkts) {
+	BufArray<mbuf> *inPktsBA = reinterpret_cast<BufArray<mbuf> *>(obj);
+
+	inPktsBA->getSendBufs(reinterpret_cast<mbuf **>(sendPkts));
+	inPktsBA->getFreeBufs(reinterpret_cast<mbuf **>(freePkts));
+
+	delete (inPktsBA);
+};
+
+void *HelloBye3MemPool_Client_process(void *obj, struct rte_mbuf **inPkts,
+	unsigned int inCount, unsigned int *sendCount, unsigned int *freeCount) {
+	BufArray<mbuf> *inPktsBA =
+		new BufArray<mbuf>(reinterpret_cast<mbuf **>(inPkts), inCount, true);
+
+	auto *sm =
+		reinterpret_cast<StateMachine<HelloBye3MemPool::Identifier<mbuf>, mbuf> *>(obj);
+	sm->runPktBatch(*inPktsBA);
+	*sendCount = inPktsBA->getSendCount();
+	*freeCount = inPktsBA->getFreeCount();
+
+	return inPktsBA;
+};
+
+void HelloBye3MemPool_Client_free(void *obj) {
+	delete (reinterpret_cast<StateMachine<HelloBye3MemPool::Identifier<mbuf>, mbuf> *>(obj));
+	rte_mempool_free(HelloBye3MemPool::Client::mempool);
+};
+};
+#endif /* HELLOBYE3PROTO_CPP */
