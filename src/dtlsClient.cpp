62eaf4ebbedb404c1f09d11cf214b5b973fec1c0
Dominik Schoeffmann <schoeffm@in.tum.de>
2018-02-13 14:38:55 +0100

    handle exceptions in dtlsClient.cpp

--- src/dtlsClient.cpp
+++ src/dtlsClient.cpp
@@ -97,7 +97,8 @@ static int writeAllDataAvailable(dtlsClient *client, mbuf *pkt, SM::FunIface &fu
 	// Is there data to write? (There should be)
 	if (BIO_eof(client->wbio) == false) {
 
-		DEBUG_ENABLED(std::cout << "DTLS_Client::writeAllDataAvailable() sending packet" << std::endl;)
+		DEBUG_ENABLED(
+			std::cout << "DTLS_Client::writeAllDataAvailable() sending packet" << std::endl;)
 
 		Headers::Ethernet *ethernet = reinterpret_cast<Headers::Ethernet *>(pkt->getData());
 		Headers::IPv4 *ipv4 = reinterpret_cast<Headers::IPv4 *>(ethernet->getPayload());
@@ -133,8 +134,8 @@ static int writeAllDataAvailable(dtlsClient *client, mbuf *pkt, SM::FunIface &fu
 	// Maybe there is more data to send, therefore request extra packets if need be
 	while (BIO_eof(client->wbio) == false) {
 
-		DEBUG_ENABLED(std::cout << "DTLS_Client::writeAllDataAvailable() sending extra packet" << std::endl;)
-
+		DEBUG_ENABLED(std::cout << "DTLS_Client::writeAllDataAvailable() sending extra packet"
+								<< std::endl;)
 
 		// Try to get an extra packet
 		mbuf *xtraPkt = funIface.getPkt();
@@ -204,7 +205,8 @@ void runHandshake(SM::State &state, mbuf *pkt, SM::FunIface &funIface) {
 	// Check if the handshake is finished
 	if (SSL_is_init_finished(client->ssl)) {
 		funIface.transition(States::ESTABLISHED);
-		DEBUG_ENABLED(std::cout << "DTLS_Client::runHandshake() Connection established" << std::endl;)
+		DEBUG_ENABLED(
+			std::cout << "DTLS_Client::runHandshake() Connection established" << std::endl;)
 
 		int allHeaderLength =
 			sizeof(Headers::Ethernet) + sizeof(Headers::IPv4) + sizeof(Headers::Udp);
@@ -309,73 +311,107 @@ struct Dtls_C_config {
 };
 
 void *DtlsClient_init(uint32_t dstIP, uint16_t dstPort) {
-	auto *obj = new StateMachine<IPv4_5TupleL2Ident<mbuf>, mbuf>();
+	try {
+		auto *obj = new StateMachine<IPv4_5TupleL2Ident<mbuf>, mbuf>();
 
-	DTLS_Client::configStateMachine(*obj);
+		DTLS_Client::configStateMachine(*obj);
 
-	struct Dtls_C_config *ret = new Dtls_C_config();
-	ret->dstIP = dstIP;
-	ret->dstPort = dstPort;
-	ret->ctx = DTLS_Client::createCTX();
-	ret->sm = obj;
+		struct Dtls_C_config *ret = new Dtls_C_config();
+		ret->dstIP = dstIP;
+		ret->dstPort = dstPort;
+		ret->ctx = DTLS_Client::createCTX();
+		ret->sm = obj;
 
-	return ret;
+		return ret;
+	} catch (std::exception *e) {
+		std::cout << "DtlsClient_init() caught exception:" << std::endl
+				  << e->what() << std::endl;
+		std::abort();
+	}
 };
 
 void *DtlsClient_connect(void *obj, struct rte_mbuf **inPkts, unsigned int inCount,
 	unsigned int *sendCount, unsigned int *freeCount, uint32_t srcIP, uint16_t srcPort) {
 
-	auto config = reinterpret_cast<Dtls_C_config *>(obj);
+	try {
+		auto config = reinterpret_cast<Dtls_C_config *>(obj);
 
-	BufArray<mbuf> *inPktsBA =
-		new BufArray<mbuf>(reinterpret_cast<mbuf **>(inPkts), inCount, true);
+		BufArray<mbuf> *inPktsBA =
+			new BufArray<mbuf>(reinterpret_cast<mbuf **>(inPkts), inCount, true);
 
-	IPv4_5TupleL2Ident<mbuf>::ConnectionID cID;
-	cID.dstIP = htonl(srcIP);
-	cID.srcIP = htonl(config->dstIP);
-	cID.dstPort = htons(srcPort);
-	cID.srcPort = htons(config->dstPort);
-	cID.proto = Headers::IPv4::PROTO_UDP;
+		IPv4_5TupleL2Ident<mbuf>::ConnectionID cID;
+		cID.dstIP = htonl(srcIP);
+		cID.srcIP = htonl(config->dstIP);
+		cID.dstPort = htons(srcPort);
+		cID.srcPort = htons(config->dstPort);
+		cID.proto = Headers::IPv4::PROTO_UDP;
 
-	auto state = DTLS_Client::createStateData(
-		config->ctx, srcIP, config->dstIP, srcPort, config->dstPort);
+		auto state = DTLS_Client::createStateData(
+			config->ctx, srcIP, config->dstIP, srcPort, config->dstPort);
 
-	config->sm->addState(cID, state, *inPktsBA);
+		config->sm->addState(cID, state, *inPktsBA);
 
-	*sendCount = inPktsBA->getSendCount();
-	*freeCount = inPktsBA->getFreeCount();
+		*sendCount = inPktsBA->getSendCount();
+		*freeCount = inPktsBA->getFreeCount();
 
-	return inPktsBA;
+		return inPktsBA;
+
+	} catch (std::exception *e) {
+		std::cout << "DtlsClient_connect() caught exception:" << std::endl
+				  << e->what() << std::endl;
+		std::abort();
+	}
 };
 
 void DtlsClient_getPkts(void *obj, struct rte_mbuf **sendPkts, struct rte_mbuf **freePkts) {
-	BufArray<mbuf> *inPktsBA = reinterpret_cast<BufArray<mbuf> *>(obj);
+	try {
+		BufArray<mbuf> *inPktsBA = reinterpret_cast<BufArray<mbuf> *>(obj);
 
-	inPktsBA->getSendBufs(reinterpret_cast<mbuf **>(sendPkts));
-	inPktsBA->getFreeBufs(reinterpret_cast<mbuf **>(freePkts));
+		inPktsBA->getSendBufs(reinterpret_cast<mbuf **>(sendPkts));
+		inPktsBA->getFreeBufs(reinterpret_cast<mbuf **>(freePkts));
 
-	delete (inPktsBA);
+		delete (inPktsBA);
+
+	} catch (std::exception *e) {
+		std::cout << "DtlsClient_getPkts() caught exception:" << std::endl
+				  << e->what() << std::endl;
+		std::abort();
+	}
 };
 
 void *DtlsClient_process(void *obj, struct rte_mbuf **inPkts, unsigned int inCount,
 	unsigned int *sendCount, unsigned int *freeCount) {
-	BufArray<mbuf> *inPktsBA =
-		new BufArray<mbuf>(reinterpret_cast<mbuf **>(inPkts), inCount, true);
+	try {
+		BufArray<mbuf> *inPktsBA =
+			new BufArray<mbuf>(reinterpret_cast<mbuf **>(inPkts), inCount, true);
 
-	auto config = reinterpret_cast<Dtls_C_config *>(obj);
+		auto config = reinterpret_cast<Dtls_C_config *>(obj);
 
-	config->sm->runPktBatch(*inPktsBA);
-	*sendCount = inPktsBA->getSendCount();
-	*freeCount = inPktsBA->getFreeCount();
+		config->sm->runPktBatch(*inPktsBA);
+		*sendCount = inPktsBA->getSendCount();
+		*freeCount = inPktsBA->getFreeCount();
 
-	return inPktsBA;
+		return inPktsBA;
+
+	} catch (std::exception *e) {
+		std::cout << "DtlsClient_process() caught exception:" << std::endl
+				  << e->what() << std::endl;
+		std::abort();
+	}
 };
 
 void DtlsClient_free(void *obj) {
-	auto config = reinterpret_cast<Dtls_C_config *>(obj);
+	try {
+		auto config = reinterpret_cast<Dtls_C_config *>(obj);
 
-	delete (config->sm);
-	OPENSSL_free(config->ctx);
-	delete (config);
+		delete (config->sm);
+		OPENSSL_free(config->ctx);
+		delete (config);
+
+	} catch (std::exception *e) {
+		std::cout << "DtlsClient_free() caught exception:" << std::endl
+				  << e->what() << std::endl;
+		std::abort();
+	}
 };
 };
