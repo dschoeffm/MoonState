0df512facbd3638db9e6adc8e2d2e1bfe524f337
Dominik Schoeffmann <schoeffm@in.tum.de>
2018-06-13 07:47:44 +0200

    update runStateTableSize.py

--- benchmarks/runStateTableSize.py
+++ benchmarks/runStateTableSize.py
@@ -13,10 +13,10 @@ rerunTimes = 128
 
 #cur = step
 
-print("size,numMemAccesses,setup,run")
+print("size,rerun,setup,run")
 
 points = [ 2**(startPow + x*((maxValPow - startPow)/steps)) for x in range(1,steps) ]
-numMemAccesses = [ x for x in range(0,128) ]
+numMemAccesses = [ x for x in range(0,1) ]
 
 #print(points)
 
@@ -25,17 +25,14 @@ for cur in points:
 		rerunTimes = rerunTimes - 4
 
 	for n in numMemAccesses:
-		curSetup = 999999999999999
-		curRun = 999999999999999
 
 		for x in range(0,rerunTimes):
 			proc = subprocess.run(['./stateTableSize',str(cur),str(n)],stdout=subprocess.PIPE)
 			strings = proc.stdout.decode('utf-8').split('\n')
 
-			curSetup = min(int(strings[0].split(' ')[1]), curSetup)
-			curRun = min(int(strings[1].split(' ')[1]), curRun)
+			curSetup = int(strings[0].split(' ')[1])
+			curRun = int(strings[1].split(' ')[1])
 
-		curSetup = (curSetup * 1.0) / cur
-		curRun = (curRun * 1.0) / cur
-		print(str(cur) + ',' + str(n) + ',' + str(curSetup) + ',' + str(curRun))
-#	cur = cur + step
+			curSetup = (curSetup * 1.0) / cur
+			curRun = (curRun * 1.0) / cur
+			print(str(cur) + ',' + str(x) + ',' + str(curSetup) + ',' + str(curRun))
